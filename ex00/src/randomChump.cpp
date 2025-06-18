c2746a70c6dcfbb3b6218d8e34efbc88792e208c
Alimat Okhapkina <aokhapki@2-H-9.42heilbronn.de>
2025-06-18 17:17:47 +0200

    add a function into randomChump.cpp

--- ex00/src/randomChump.cpp
+++ ex00/src/randomChump.cpp
@@ -6,8 +6,12 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 04:01:41 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/05/12 04:01:44 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/06/18 17:16:37 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+void randomChamp(std::string name)
+{
+	Zombie zombie(name);
+	zombie.announce();}
