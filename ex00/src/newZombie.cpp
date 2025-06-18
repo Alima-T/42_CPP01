fa36c97592bd51b51d0e02095fd83479162131ef
Alimat Okhapkina <aokhapki@2-H-9.42heilbronn.de>
2025-06-18 17:15:19 +0200

    add a function into newZombie.cpp

--- ex00/src/newZombie.cpp
+++ ex00/src/newZombie.cpp
@@ -6,8 +6,13 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 03:31:30 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/05/12 04:18:41 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/06/18 17:14:30 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie* newZombie(std::string name)
+{
+	return new Zombie(name);
+};
