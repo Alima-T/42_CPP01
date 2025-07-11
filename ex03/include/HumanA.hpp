9be05d83dfc752a20d0637112ec2088d191b8272
Alimat Okhapkina <aokhapki@2-C-8.42heilbronn.de>
2025-07-11 16:51:49 +0200

    add Makefile and headder for ex03

---
+++ ex03/include/HumanA.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 01:32:10 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/07/11 01:56:08 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include<string>
+#include "Weapon.hpp"
+class HumanA
+{
+	private:
+	std::string m_name;
+	Weapon& weapon; // reference: always has a weapon
+
+	public:
+	HumanA(const std::string& name, Weapon& weapon);
+	void attack() const;
+};
+
+
+#endif
