ca7bc4b887017bd0b004d42a0fc14c1ab3dfd67f
Alimat Okhapkina <aokhapki@2-A-6.42heilbronn.de>
2025-07-18 21:57:28 +0200

    add main

---
+++ ex03/src/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 21:04:41 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/07/15 21:34:25 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+#include <string>
+
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club"); //грубая дубина с шипами
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+
+	return 0;
+}
